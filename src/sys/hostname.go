ee9af7771bf69d829012992b3f747c245e931c8e
Денис Туляков <dtulyakov@gmail.com>
2017-02-09 23:35:53 +0300

    На ветке master 	изменено:      src/sys/hostname.go

--- src/sys/hostname.go
+++ src/sys/hostname.go
@@ -20,5 +20,9 @@ func main() {
 
     fmt.Printf("Это лять hostname %s\n", hostname)
     fmt.Printf("Это лять out %s\n", out)
+    t := time.Now()
+    fmt.Println(t.Format("2006-01-02"))
+//    fmt.Println(t.Format("2006-01-02_15-04-05"))
+
 }
 
